768fbb52b5a9e1f70eab186374a1f56fdedde3ee
quedvi <quedvi@gmx.at>
2022-05-29 13:32:37 +0200

    cleanup annotations

--- qoi.py
+++ qoi.py
@@ -1,27 +1,27 @@
-from dataclasses import dataclass
+import array
 import numpy as np
 from PIL import Image
 
 # encoder/decoder for lossloss image file format: https://qoiformat.org/
 # ./test_images also from https://qoiformat.org/
 
-@dataclass
 class Pixel:
-    r: int
-    g: int
-    b: int
-    a: int
+    def __init__(self, r, g, b, a) -> 'Pixel':
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
 
     def __repr__(self) -> str:
         return f"Pixel: ({self.r}, {self.g}, {self.b}, {self.a})"
 
-    def diff(self, decode_value):
+    def diff(self, decode_value) -> 'Pixel':
         r = (self.r + (((decode_value >> 4) & 0b11) - 2)) % 256
         g = (self.g + (((decode_value >> 2) & 0b11) - 2)) % 256 
         b = (self.b + ((decode_value        & 0b11) - 2)) % 256
         return Pixel(r, g, b, self.a)
     
-    def diff_luma(self, dg, byte):
+    def diff_luma(self, dg, byte) -> 'Pixel':
         nr = byte >> 4
         nb = byte & 0b1111
         ng = dg - 32
@@ -30,15 +30,10 @@ class Pixel:
         b = (self.b + ng - 8 + nb) % 256
         return Pixel(r, g, b, self.a)
 
-    def hash(self):
+    def hash(self) -> int:
         return ((self.r * 3 + self.g * 5 + self.b * 7 + self.a * 11) % 64)
 
-@dataclass
 class Qoi:
-    file: str
-    header: dict
-    image: list
-
     QOI_OP_RGB  = 0b11111110 # 254
     QOI_OP_RGBA = 0b11111111 # 255
 
@@ -47,18 +42,19 @@ class Qoi:
     QOI_OP_LUMA  = 0b10 # 2
     QOI_OP_RUN   = 0b11 # 3
 
-    def __init__(self, file_name):
+    def load(self, file_name) -> 'Qoi':
         self.header = { "name": file_name }
-        
-    def load(self):
         self.file = open(self.header['name'], "rb")
+
         header = bytearray(self.file.read(14))
         if header[0:4] != b'qoif': return None
+
         self.header["head"      ] = b'qoif'
         self.header["width"     ] = header[4] * 16**6 + header[5] * 16**4 + header[6]  * 16**2 + header[7]
         self.header["height"    ] = header[8] * 16**6 + header[9] * 16**4 + header[10] * 16**2 + header[11]
         self.header["channels"  ] = header[12]
         self.header["colorspace"] = header[13]
+
         self.image = []
         self.__decode()
         return self
@@ -75,10 +71,10 @@ class Qoi:
     def channels(self) -> int:
         return self.header['channels']
 
-    def image(self):
+    def image(self) -> list:
         return self.image
 
-    def image_data(self):
+    def image_data(self) -> np.array:
         if self.channels() == 3:
             data = [np.array([np.uint8(x.r), np.uint8(x.g), np.uint8(x.b)]) for x in self.image]
         elif self.channels() == 4:
@@ -86,14 +82,14 @@ class Qoi:
         array = np.array(data).reshape(self.height(), self.width(), self.channels())
         return array
 
-    def __read_byte(self):
+    def __read_byte(self) -> int:
         byte = self.file.read(1)
         if byte == b'':
             return None
         else:
             return int.from_bytes(byte, 'little')
        
-    def __read_bytes(self, count):
+    def __read_bytes(self, count) -> list:
         read = []
         for i in range(count):
             byte = self.file.read(1)
@@ -103,7 +99,7 @@ class Qoi:
             
         return [int.from_bytes(x, 'little') for x in read]
 
-    def __decode(self):
+    def __decode(self) -> None:
         image_size = self.width() * self.height() * self.channels()
         decoded_image = []
         
@@ -145,6 +141,10 @@ class Qoi:
         self.image = decoded_image[0:-8] # discard 8 end marker
 
 
-image = Qoi("./test_images/testcard.qoi").load()
+image = Qoi().load("./test_images/testcard.qoi")
 i=Image.fromarray(image.image_data())
 i.save("test_image.png")
+
+
+
+# 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
