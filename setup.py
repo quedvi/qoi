0ee57b03ada62a25a5a9bf907c3325d12c6a5ceb
Martin Oehzelt <quedvi@gmx.at>
2024-10-08 09:15:15 +0200

    Update setup.py to version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='qoi-conv',
-    version='1.0.1',    
+    version='1.0.2',    
     description='A simple implementation of the QOI image format in Pyhton.',
     long_description=long_description,
     long_description_content_type="text/markdown",    
